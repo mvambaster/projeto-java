c339302a894eb8d6a73628c6117427f9f808a4e4
Michael Vambaster <45100957+mvambaster@users.noreply.github.com>
2018-11-28 10:30:49 -0200

    Update HelloWord.java

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,1 +1,9 @@
 
+package java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
